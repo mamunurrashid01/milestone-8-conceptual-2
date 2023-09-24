59d19a354b75fc3b3b3dee55d8f55f19ac04afd8
mamunur-rashid <mdmamunurrashid.webdeveloper@gmail.com>
2023-09-24 01:11:01 -0700

    basic setup

---
+++ src/Route/Route.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
+import Home from '../pages/home/Home';
+import Favourite from '../pages/Favourite/Favourite';
+import Login from '../pages/Login/Login';
+
+const myCreatedRoute = createBrowserRouter([
+    {
+        path:"/",
+        element:<MainLayout></MainLayout>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"/favourite",
+                element:<Favourite></Favourite>
+            },
+            {
+                path:"/login",
+                element:<Login></Login>
+            }
+        ]
+    }
+]);
+
+export default myCreatedRoute;
